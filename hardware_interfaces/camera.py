cbcb5aaaf0420d65c1e4ba698dab099424605f1c
Joe Miller 6 <gstajoe@gmail.com>
2017-03-09 22:54:26 +0000

    Add camera works

---
+++ hardware_interfaces/camera.py
@@ -0,0 +1,11 @@
+import picamera
+import time
+import datetime
+
+camera = picamera.PiCamera()
+
+# take an image every hour 
+while True:
+	image_title = '{timestamp}.jpg'.format(timestamp=time.time()) 
+	camera.capture(image_title)
+	time.sleep(60)
